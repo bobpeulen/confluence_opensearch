a49990c9da1eb2b71ab337de31614544d49cd79f
Bob Peulen <75362416+bobpeulen@users.noreply.github.com>
2025-01-08 12:04:39 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+###### Set configuration parameters
+######
+######
+
+
+#### Confluence parameters
+confluence_space_id = "x"
+full_confluence_url = "https://xx.atlassian.net"
+username_confluence="x@gmail.com"
+atlassian_api_token = "xx"
+
+
+#### Set OCI OpenSearch parameters
+host = "x.opensearch.eu-frankfurt-1.oci.oraclecloud.com"
+username = "x"
+password = ""
