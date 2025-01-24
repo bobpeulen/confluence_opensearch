f26044dcc433cbab36dcc8ac0e1d29a5e6f5ffc2
Bob Peulen <75362416+bobpeulen@users.noreply.github.com>
2025-01-24 16:28:09 +0100

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,15 @@
 
 
 #### Confluence parameters
-confluence_space_id = "x"
-full_confluence_url = "https://xx.atlassian.net"
-username_confluence="x@gmail.com"
-atlassian_api_token = "xx"
+confluence_space_id = "x712330240cc8c6b8efdaa4870906c57cfca54bc86"
+full_confluence_url = "https://peulenbob.atlassian.net"
+username_confluence="peulenbob@gmail.com"
+atlassian_api_token = "ATATT3mlIdkl8A-SOGfBrBQGLE3pE81Kh5kC5fnOkXCVQDaGkYBA2N9Muh4AfqsnWxdyEIGk-B6lIHKBgMLJiIPTWZ__V0c1CyZt-iIFwp5ekTDh6jyqeJCsDvAw=0D2A5605"
 
 
-#### Set OCI OpenSearch parameters
-host = "x.opensearch.eu-frankfurt-1.oci.oraclecloud.com"
-username = "x"
+#### Set OCI OpenSearch parameters. Full API Endpoint
+host = "https://amaaaaaaeicj2tiapempxdffke77vfvuy4e3xtcnoscpb5qitk2sidpjeyxa.opensearch.us-ashburn-1.oci.oraclecloud.com:9200"
+username = "adminx"
 password = ""
+
+index_name = "confluence_embeddings_v2"
