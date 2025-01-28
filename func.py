43136fbb28b41fc98570b538351e46049833d839
Bob Peulen <75362416+bobpeulen@users.noreply.github.com>
2025-01-28 14:17:38 +0100

    Update func.py

--- func.py
+++ func.py
@@ -1,64 +1,30 @@
-
 import io
 import json
-from fdk import response
-from atlassian import Confluence
-from bs4 import BeautifulSoup
-import datetime
-from datetime import datetime
-import tiktoken
-from langchain.vectorstores import OpenSearchVectorSearch
-from langchain_huggingface import HuggingFaceEmbeddings
+import logging
 import oci
-import ads
-from tqdm import tqdm
-
-#load configuration
-from config import confluence_space_id, full_confluence_url, username_confluence, atlassian_api_token, host, username, password, index_name
-
-#Load all customer functions
-from helpers import create_embedding_model
-from helpers import ingest_documents_with_embeddings
-from helpers import chunk_text
-from helpers import parse_page
-from helpers import create_confluence_client
-from helpers import create_opensearch_client
+import requests
+from fdk import response
 
 
-def handler(ctx, data: io.BytesIO=None):
-    
-    page_id = "no_page_id_available"
-    page_url = "no_page_url_available"
-
+def handler(ctx, data: io.BytesIO = None):
+    auth = oci.auth.signers.get_resource_principals_signer()
+    logger = logging.getLogger()
     try:
-        
-        ## establish connections
-        confluence_client = create_confluence_client(full_confluence_url, username_confluence, atlassian_api_token)
-        oci_opensearch_client = create_opensearch_client(host, username, password)
-        
-        #get the body and values
+        logger.info("Inside function")
         body = json.loads(data.getvalue())
-        print(f"Printing the body {body}")
-        page_id = body.get("page_id")
-        page_url = body.get("page_url")
-        print(f"Printing the page_id {page_id}")
-        print(f"Printing the page_url {page_url}")
-        
-        # parse the page and create chunks
-        chunks = parse_page(confluence_client, page_id)
-        
-        #parse chunks of text into embeddings
-        embedding_model = create_embedding_model()
-        
-        # convert chunks into embeddings and push to OCI OpenSearch.
-        ingest_documents_with_embeddings(chunks=chunks, index_name=index_name, oci_opensearch_client=oci_opensearch_client, host=host, username=username, password=password, embedding_model=embedding_model, batch_size=5)
-        
-    except (Exception, ValueError) as ex:
-        print(str(ex), flush=True)
-        print("Not loaded any data")
+        logger.info("Body : " + json.dumps(body))
+        headers = ctx.Headers()
+        logger.info("Headers: " + json.dumps(headers))
 
+        #get http endpoint from body        
+        endpoint = body.get("model_deployment_http_endpoint")
+
+        #send the full body
+        resp = requests.post(endpoint, json=body, auth=auth)
+        logger.info("response : " + resp.json())
+    except (Exception, ValueError) as ex:
+        logger.error("Failed to call endpoint with ex : {}".format(str(ex)))
     return response.Response(
-        ctx, response_data=json.dumps(
-            {"message": "The changed page id is {0}".format(page_id)}),
-        headers={"Content-Type": "application/json"}
-    )
+            ctx, response_data=resp.json(),
+            headers={"Content-Type": "application/json"}
+        )
